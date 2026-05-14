b0e5f31420787ed47e66442ed25024957998ff36
mkavulu <mutukukavulu2000@gmail.com>
2026-05-14 16:40:49 +0300

    made changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://smart-expense-tracker-ba8v.onrender.com/api/", // Render backend
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
+// Attach JWT token automatically
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
